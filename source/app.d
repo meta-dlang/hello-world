eba9299e2c37de4264335c4d53e96819876e0cd5
hatf0 <harrison@0xcc.pw>
2021-07-20 20:19:38 -0400

    update message further

--- source/app.d
+++ source/app.d
@@ -1,9 +1,11 @@
 import std.stdio;
 import std.compiler;
+version(LDC) import ldc.intrinsics;
 
 void main()
 {
 	writeln("Hello from Yocto, D!");
-    writeln("Compiler name: ", std.compiler.name);
-    writeln("Compiler version: v", std.compiler.version_major, ".", std.compiler.version_minor); 
+    writef("Compiler name: %s\n", std.compiler.name);
+    writef("Compiler version: v%1d.%03d\n", std.compiler.version_major, std.compiler.version_minor); 
+    version(LDC) writef("LLVM version: %d\n", ldc.intrinsics.LLVM_version);
 }
